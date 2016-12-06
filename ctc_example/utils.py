eaa91db5d979e47538db353ec1d978c79007c709
Yimeng Zhang <zym1010@gmail.com>
2016-12-06 01:19:11 -0500

    add siamese data

--- ctc_example/utils.py
+++ ctc_example/utils.py
@@ -1,4 +1,7 @@
 import numpy as np
+import os
+import os.path
+from itertools import combinations
 
 
 def target_list_to_sparse_tensor(targetList):
@@ -59,7 +62,6 @@ def data_lists_to_batches(inputList, targetList, batchSize):
 
 
 def load_batched_data(specPath, targetPath, batchSize):
-    import os
     '''returns 3-element tuple: batched data (list), max # of time steps (int), and
        total number of samples (int)'''
     dir_list_1 = sorted(os.listdir(specPath))
@@ -72,3 +74,87 @@ def load_batched_data(specPath, targetPath, batchSize):
                                  [np.load(os.path.join(targetPath, fn)) for fn in dir_list_2],
                                  batchSize) + \
            (len(dir_list_1),)
+
+
+def load_batched_data_sentence(featurePath, sentencePath, seed=0, neg_pair_for_each_positive_pair=2):
+    dir_list_1 = sorted(os.listdir(featurePath))
+    dir_list_2 = sorted(os.listdir(sentencePath))
+    assert dir_list_1 == dir_list_2
+
+    for x in dir_list_1:
+        assert x.endswith('.npy')
+
+    sentence_id_list = [
+        x.split('/')[-1].split('_')[-1][:-4] for x in dir_list_1
+        ]
+    # print(sentence_id_list)
+
+    feature_all = [np.load(os.path.join(featurePath, fn)) for fn in dir_list_1]
+    sentence_id_all = np.array([np.load(os.path.join(sentencePath, fn)) for fn in dir_list_1])
+    assert sentence_id_all.shape == (len(feature_all),)
+    loc_all = np.arange(len(feature_all))
+
+    rng_state = np.random.RandomState(seed=seed)
+
+    # ok. let's create
+    unique_sentence_ids, sentence_id_counts = np.unique(sentence_id_all, return_counts=True)
+
+    positive_sentence_mask = sentence_id_counts > 1
+    print('unique id count {},'
+          'positive set {}'.format(unique_sentence_ids.size, positive_sentence_mask.sum()))
+    assert (sentence_id_counts[positive_sentence_mask] == 7).all() and positive_sentence_mask.sum() == 330
+
+    positive_unique_sentence_ids = unique_sentence_ids[positive_sentence_mask]
+
+    # I will iterate all positive pairs. For each positive pair, I will create (roughly) equal number of negative pairs
+    # for each of two, with sum of negative pairs equal neg_pair_for_each_positive_pair.
+    assert neg_pair_for_each_positive_pair > 1
+    neg_pair_for_l = neg_pair_for_each_positive_pair // 2
+    neg_pair_for_r = neg_pair_for_each_positive_pair - neg_pair_for_l
+
+    signal_list_all = []
+    feature_list_left_all = []
+    feature_list_right_all = []
+
+    for pos_id in positive_unique_sentence_ids:
+        # first, find all indices equal to this.
+        positive_loc_this = loc_all[sentence_id_all == pos_id]
+        negative_loc_this = loc_all[sentence_id_all != pos_id]
+
+        # then, do a combination of all pairs in postive_loc_this.
+        for pos_pair_l, pos_pair_r in combinations(positive_loc_this, 2):
+            # then choose neg_pair_for_l for left
+
+            neg_pair_list = rng_state.choice(negative_loc_this, size=neg_pair_for_each_positive_pair,
+                                             replace=True)
+            neg_pair_list_l = neg_pair_list[:neg_pair_for_l]
+            neg_pair_list_r = neg_pair_list[neg_pair_for_l:]
+            assert neg_pair_list_l.shape == (neg_pair_for_l,)
+            assert neg_pair_list_r.shape == (neg_pair_for_r,)
+
+            # first emit this positive pair
+            pair_list = [(pos_pair_l, pos_pair_r)] + [(pos_pair_l, x) for x in neg_pair_list_l] + [(pos_pair_r, x) for x
+                                                                                                   in neg_pair_list_r]
+            pair_name_list = [(sentence_id_list[l], sentence_id_list[r]) for (l, r) in pair_list]
+            # print(pair_list)
+            # print(pair_name_list)
+            for p in pair_list:
+                assert p[0] != p[1]
+            for p in pair_name_list[:1]:
+                assert p[0] == p[1]
+            for p in pair_name_list[1:]:
+                assert p[0] != p[1]
+            signal_list_this = np.concatenate((np.ones(1, dtype=np.bool_),
+                                               np.zeros(neg_pair_for_each_positive_pair, dtype=np.bool_)))
+            assert signal_list_this.shape == (len(pair_list),) == (len(pair_name_list),)
+
+            signal_list_all.append(signal_list_this)
+            feature_list_left_all.extend([feature_all[l] for (l, _) in pair_list])
+            feature_list_right_all.extend([feature_all[r] for (_, r) in pair_list])
+
+    signal_list_all = np.concatenate(signal_list_all)
+    pos_pair_total = 330 * 7 * 6 // 2 * (1 + neg_pair_for_each_positive_pair)
+    assert signal_list_all.shape == (pos_pair_total,)
+    assert len(feature_list_left_all) == len(feature_list_right_all) == pos_pair_total
+
+    return feature_list_left_all, feature_list_right_all, signal_list_all
